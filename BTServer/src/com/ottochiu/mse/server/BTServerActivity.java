9f44ba4546c05e61d762c787fb632aef983f22ee
Otto Chiu <ottochiu@gmail.com>
2012-01-31 22:52:18 -0800

    Initial

---
+++ BTServer/src/com/ottochiu/mse/server/BTServerActivity.java
@@ -0,0 +1,13 @@
+package com.ottochiu.mse.server;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BTServerActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
