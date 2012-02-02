72e5f11ca870177f2f122e2ab781cdd0b7ec7eb0
Otto Chiu <ottochiu@gmail.com>
2012-02-01 21:53:52 -0800

    update

--- heartrate.php
+++ heartrate.php
@@ -19,11 +19,25 @@ mysql_select_db($database) or die("Cannot select database");
 
 <form method="post" action="heartrate.php">
 
-
-
+  Please enter record number (0 to view all): <input type="text" size="3" name="record_number"/>
+  <br />
+  <input type="submit">
 </form>
 
-Please enter record number (0 to view all): <input type="text" size="3" name="record_number"/>
+<?php 
+
+// A form has been submitted. Query database with input.
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+
+	$id = $_POST['record_number'];
+	
+	if (filter($id, FILTER_VALIDATE_INT) && $id > 0) {
+		echo "GOOD";
+	}
+}
+	
+?>
+
 </body>
 </html>
 
