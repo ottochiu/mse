bca8c896da02cc1cd6eae57cfcd0ed41ae123d92
Otto Chiu <ottochiu@gmail.com>
2012-02-01 22:00:59 -0800

    db

--- heartrate.php
+++ heartrate.php
@@ -8,6 +8,21 @@ $database = "ottochiu";
 mysql_connect($server, $user, $password) or die("Cannot connect to localhost");
 mysql_select_db($database) or die("Cannot select database");
 
+// A form has been submitted. Query database with input.
+$query = "SELECT * FROM mse_heartrate_interval ";
+
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+
+	$id = $_POST['record_number'];
+
+	if (filter_var($id, FILTER_VALIDATE_INT) && $id > 0) {
+		// Has been validated to be an int. Don't need to escape
+		$query .= sprintf(" WHERE session_id=%d ORDER BY id", $id);
+	}
+}
+
+echo $query;
+
 ?>
 
 <html>
@@ -19,22 +34,12 @@ mysql_select_db($database) or die("Cannot select database");
 
 <form method="post" action="heartrate.php">
 
-  Please enter record number (0 to view all): <input type="text" size="3" name="record_number"/>
+  Please enter record number (0 to view all): <input type="text" size="3" name="record_number" value="/>
   <br />
   <input type="submit">
 </form>
 
 <?php 
-
-// A form has been submitted. Query database with input.
-if ($_SERVER['REQUEST_METHOD'] == "POST") {
-
-	$id = $_POST['record_number'];
-	
-	if (filter($id, FILTER_VALIDATE_INT) && $id > 0) {
-		echo "GOOD";
-	}
-}
 	
 ?>
 
