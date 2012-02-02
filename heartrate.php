06f6313e3e6abc7ec3189ef2c14d1b30cd67c906
Otto Chiu <ottochiu@gmail.com>
2012-02-01 21:22:41 -0800

    connect db

---
+++ heartrate.php
@@ -0,0 +1,10 @@
+<?php 
+
+$user = 'ottochiu';
+$password = 'qtEciLiz';
+$database = 'ottochiu';
+
+mysql_connect(localhost,$username,$password) or die "Cannot connect to localhost";
+mysql_select_db($database) or die "Cannot select database";
+
+?>
