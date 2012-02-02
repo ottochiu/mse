c46d9643006d8b6da0591a70e33431749a0020da
Otto Chiu <ottochiu@gmail.com>
2012-02-01 21:41:39 -0800

    php

--- heartrate.php
+++ heartrate.php
@@ -1,10 +1,29 @@
 <?php 
 
-$user = 'ottochiu';
-$password = 'qtEciLiz';
-$database = 'ottochiu';
+$server = "mysql.ottochiu.vaelen.org";
+$user = "ottochiu";
+$password = "qtEciLiz";
+$database = "ottochiu";
 
-mysql_connect(localhost,$username,$password) or die "Cannot connect to localhost";
-mysql_select_db($database) or die "Cannot select database";
+mysql_connect($server, $user, $password) or die("Cannot connect to localhost");
+mysql_select_db($database) or die("Cannot select database");
 
 ?>
+
+<html>
+<head>
+	<title>Heart rate history</title>
+</head>
+
+<body>
+
+<form method="post" action="heartrate.php">
+
+
+
+</form>
+
+Please enter record number (0 to view all): <input type="text" size="3" name="record_number"/>
+</body>
+</html>
+
