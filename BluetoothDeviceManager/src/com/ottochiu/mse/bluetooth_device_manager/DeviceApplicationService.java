087451086a2b170eef79a63f25e424cae3ac8f42
Otto Chiu <ottochiu@gmail.com>
2012-02-28 17:02:09 -0800

    AIDL continued

--- BluetoothDeviceManager/src/com/ottochiu/mse/bluetooth_device_manager/DeviceApplicationService.java
+++ BluetoothDeviceManager/src/com/ottochiu/mse/bluetooth_device_manager/DeviceApplicationService.java
@@ -3,9 +3,8 @@ package com.ottochiu.mse.bluetooth_device_manager;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
-import android.os.RemoteException;
 
-class DeviceApplicationService extends Service implements IDeviceApplicationService {
+class DeviceApplicationService extends Service {
 
 	@Override
 	public IBinder onBind(Intent arg0) {
@@ -16,18 +15,5 @@ class DeviceApplicationService extends Service implements IDeviceApplicationServ
 	@Override
 	public int onStartCommand(Intent intent, int flags, int startId) {
 		return START_STICKY;
-	}
-
-	@Override
-	public IBinder asBinder() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void registerDevice(String deviceName, String uuid,
-			String packageName) throws RemoteException {
-		// TODO Auto-generated method stub
-		
 	}
 }
